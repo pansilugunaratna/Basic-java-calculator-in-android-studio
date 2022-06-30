ca79a76956b88ff99ccb72ef0f8310efd7e260c8
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2022-06-30 23:00:14 +0530

    add subtraction page

--- app/src/main/java/lk/ucsc/calculator/MainActivity.java
+++ app/src/main/java/lk/ucsc/calculator/MainActivity.java
@@ -14,24 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Call the add function when add button clicked
         Button addButton = findViewById(R.id.addBtn);
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 gotoAdd();
-//                generateText();
             }
         });
 
-//        Button subBtn = findViewById(R.id.subBtn);
+        //Call the subtract function when subtract button clicked
+        Button subBtn = findViewById(R.id.subBtn);
 
-//        subBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                gotoSub();
-//            }
-//        });
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoSub();
+            }
+        });
     }
 
 
@@ -39,31 +40,30 @@ public class MainActivity extends AppCompatActivity {
     public void gotoAdd() {
         EditText input_1 = findViewById(R.id.input_1);
         EditText input_2 = findViewById(R.id.input_2);
+
+        //Convert inputs to integer type
         int in1 = Integer.parseInt(String.valueOf(input_1.getText()));
         int in2 = Integer.parseInt(String.valueOf(input_2.getText()));
         int toParse = in1 + in2;
         Intent intent = new Intent(this, Addition.class);
+        //Send output value as argument of string type
         intent.putExtra("outputVal1", String.valueOf(toParse));
         startActivity(intent);
     }
 
+    //Read inputs, subtract them and direct the output value to subtraction page
+    public void gotoSub() {
+        EditText input_1 = findViewById(R.id.input_1);
+        EditText input_2 = findViewById(R.id.input_2);
 
-//    public void generateText() {
-//        EditText input_1 = findViewById(R.id.input_1);
-//        EditText input_2 = findViewById(R.id.input_2);
-//        int in1 = Integer.parseInt(String.valueOf(input_1.getText()));
-//        int in2 = Integer.parseInt(String.valueOf(input_2.getText()));
-//        int toParse = in1 + in2;
-//        TextView output = findViewById(R.id.textOutput);
-//        output.setText(String.valueOf(toParse));
-//    }
+        //Convert inputs to integer type
+        int in1 = Integer.parseInt(String.valueOf(input_1.getText()));
+        int in2 = Integer.parseInt(String.valueOf(input_2.getText()));
+        int toParse = in1 - in2;
+        Intent intent = new Intent(this, Subtraction.class);
+        //Send output value as argument of string type
+        intent.putExtra("outputVal2", String.valueOf(toParse));
+        startActivity(intent);
+    }
 
-    //Direct the output value to subtraction page
-//    public void gotoSub() {
-//        EditText input = findViewById(R.id.textInput);
-//        String toParse = input.getText().toString();
-//        Intent intent = new Intent(this, Addition.class);
-//        intent.putExtra("fieldValue",toParse);
-//        startActivity(intent);
-//    }
 }
--- app/src/main/java/lk/ucsc/calculator/Subtraction.java
+++ app/src/main/java/lk/ucsc/calculator/Subtraction.java
@@ -1,26 +1,26 @@
 package lk.ucsc.calculator;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Addition extends AppCompatActivity {
+public class Subtraction extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addition);
+        setContentView(R.layout.activity_subtraction);
 
         //Get the value from the main page and declare as a string variable
-        String dataString = getIntent().getStringExtra("outputVal1");
+        String dataString = getIntent().getStringExtra("outputVal2");
 
-        TextView output = findViewById(R.id.addOutput);
-        output.setText(dataString);
+        TextView output2 = findViewById(R.id.subOutput);
+        output2.setText(dataString);
 
         //Close the activity when back button clicked
-        Button backButton1 = findViewById(R.id.backBtn1);
+        Button backButton1 = findViewById(R.id.backBtn2);
 
         backButton1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +29,4 @@ public class Addition extends AppCompatActivity {
             }
         });
     }
-
 }
